54e4efb97269a9368f39a9617b314168b1b79436
yurihaha <and_you2@naver.com>
2023-10-20 23:17:10 +0900

    feat: detail 구현, 한글이름 출력, 코드리팩토링

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { AbilityData } from '../interfaces/ abilityTypes';
+import {
+	AbilityData,
+	FlavorTextEntry,
+	Name,
+} from '../interfaces/ abilityTypes';
 
 function Detail() {
-	const { id } = useParams(); // 동적 파라미터 :id를 가져옴
-	const [abilityData, setAbilityData] = useState<AbilityData | null>(null); // AbilityData 타입으로 상태 정의
+	const { id } = useParams();
+	const [abilityData, setAbilityData] = useState<AbilityData | null>(null);
 
 	useEffect(() => {
-		// 능력 데이터를 불러오는 API 요청
 		axios
 			.get(`https://pokeapi.co/api/v2/ability/${id}/`)
-			.then((response) => {
-				const data: AbilityData = response.data; // API 응답을 AbilityData 타입으로 변환
+			.then((res) => {
+				const data: AbilityData = res.data;
 				setAbilityData(data);
 			})
 			.catch((error) => {
@@ -20,7 +23,24 @@ function Detail() {
 			});
 	}, [id]);
 
-	// 능력 데이터를 사용하여 컴포넌트를 렌더링
+	const getkoFlavorText = (
+		entries: FlavorTextEntry[] | undefined,
+	): string[] => {
+		const koreanEntries = entries
+			?.filter((entry) => entry.language.name === 'ko')
+			.map((entry) => entry.flavor_text);
+
+		return Array.from(new Set(koreanEntries || []));
+	};
+
+	const getkoNames = (names: Name[] | undefined): string[] => {
+		const koreanNames = names
+			?.filter((names) => names.language.name === 'ko')
+			.map((names) => names.name);
+
+		return Array.from(new Set(koreanNames || []));
+	};
+
 	return (
 		<div>
 			<h2>Ability Detail</h2>
@@ -29,10 +49,43 @@ function Detail() {
 					<h3>
 						포켓몬 이름: {abilityData.name} {abilityData.id}
 					</h3>
-					<p>Generation: {abilityData.generation.name}</p>
+					<br />
+					<p>EffectChange(이팩트 체인지)</p>
 					<ul>
-						{abilityData?.effect_entries.map((entry, index) => (
-							<li key={index}>{entry.effect}</li>
+						{abilityData?.effect_changes.map((entry, index) => (
+							<li key={index}>
+								{entry?.effect_entries.map((effectEntry, entryIndex) => (
+									<p key={entryIndex}>{effectEntry.effect}</p>
+								))}
+							</li>
+						))}
+					</ul>
+					<br />
+					<br />
+					<ul>
+						{getkoFlavorText(abilityData.flavor_text_entries).map(
+							(entry, index) => (
+								<li key={index}>{entry}</li>
+							),
+						)}
+					</ul>
+					{getkoFlavorText(abilityData.flavor_text_entries).length === 0 && (
+						<p>flavor 없음</p>
+					)}
+					<br />
+					<p>Generation: {abilityData.generation.name}</p>
+					<br />
+					<ul>
+						{getkoNames(abilityData.names).map((name, index) => (
+							<li key={index}>{name}</li>
+						))}
+					</ul>
+					{getkoNames(abilityData.names).length === 0 && <p>names 없음</p>}
+					<br />
+					<p>포켓몬:</p>
+					<ul>
+						{abilityData?.pokemon.map((character, index) => (
+							<li key={index}>{character.pokemon.name}</li>
 						))}
 					</ul>
 				</div>
