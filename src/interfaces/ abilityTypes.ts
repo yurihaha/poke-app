ef407614618cd396f172f6aa24a91f57d9ebddba
yurihaha <and_you2@naver.com>
2023-10-20 21:42:24 +0900

    feat: abilityTypes 타입 정리

---
+++ src/interfaces/ abilityTypes.ts
@@ -0,0 +1,57 @@
+export interface AbilityData {
+	effect_changes: EffectChange[];
+	effect_entries: EffectEntry[];
+	flavor_text_entries: FlavorTextEntry[];
+	generation: Generation;
+	id: number;
+	is_main_series: boolean;
+	name: string;
+	names: Name[];
+	pokemon: Pokemon[];
+}
+
+export interface EffectChange {
+	effect_entries: EffectEntry[];
+	version_group: VersionGroup;
+}
+
+export interface EffectEntry {
+	effect: string;
+	language: Language;
+	short_effect: string;
+}
+
+export interface FlavorTextEntry {
+	flavor_text: string;
+	language: Language;
+	version_group: VersionGroup;
+}
+
+export interface Generation {
+	name: string;
+	url: string;
+}
+
+export interface Name {
+	language: Language;
+	name: string;
+}
+
+export interface Language {
+	name: string;
+	url: string;
+}
+
+export interface VersionGroup {
+	name: string;
+	url: string;
+}
+
+export interface Pokemon {
+	is_hidden: boolean;
+	pokemon: {
+		name: string;
+		url: string;
+	};
+	slot: number;
+}
