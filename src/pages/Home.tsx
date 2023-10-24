75136b7f5fcabb85f6c7c9d63a6486ade0aaecaa
yurihaha <and_you2@naver.com>
2023-10-24 12:56:39 +0900

    feat: home 불필요한 코드 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,10 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useSetRecoilState } from 'recoil';
-import PokemonList from '../components/PokemonList';
-import { pokemonListState } from '../recoil/atom';
-
-type Pokemon = {
-	name: string;
-	url: string;
-};
-
-type PokemonList = {
-	results: Pokemon[];
-};
+import React from 'react';
+import PokemonSearch from '../components/PokemonSearch';
 
 function PokemonListApp() {
-	const setPokemonList = useSetRecoilState(pokemonListState);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await axios.get<PokemonList>(
-					'https://pokeapi.co/api/v2/pokemon?limit=10',
-				);
-				setPokemonList(res.data.results);
-			} catch (error) {
-				console.error('Error fetching data:', error);
-			}
-		};
-
-		fetchData();
-	}, [setPokemonList]);
-
 	return (
 		<div>
-			<PokemonList />
+			<PokemonSearch />
 		</div>
 	);
 }
